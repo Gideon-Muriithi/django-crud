0eb633f271ffb8ac4cf91d1cf55badd35794cd43
Gideon-Muriithi <gideongakenge@gmail.com>
2022-04-18 19:11:06 +0300

    Turn off django debug

--- CRUD/Config/settings.py
+++ CRUD/Config/settings.py
@@ -29,7 +29,7 @@ env = environ.Env(DJANGO_READ_DOT_ENV_FILE=False)
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
